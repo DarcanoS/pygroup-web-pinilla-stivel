6b5343c370e98484a11e09ab4548d5fd7e93227f
DarcanoS <spinillap@correo.udistrital.edu.co>
2020-11-05 22:33:40 -0500

    tarea 2 final

--- proyecto_flask/app/__init__.py
+++ proyecto_flask/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, Response
+#importamos de la carpeta productos y del archivo models la variable products
 from productos.models import products
 
 app = Flask(__name__)
 
 
-
+#inicializamos la variable products que es tipo Blueprint
 app.register_blueprint(products)
 
+#esto no se que hace, pero se debe colocar :'v
 if __name__ == "__main__":
     app.run(debug=True)
--- proyecto_flask/app/productos/models.py
+++ proyecto_flask/app/productos/models.py
@@ -1,10 +1,14 @@
+#Importamos el Blueprint y Response
 from flask import  Blueprint, Response
 
+#Creamos una variable tipo Blueprint, con el nombre 'products' y con la dirrecion de url '/products'
 products = Blueprint('products', __name__, url_prefix='/products')
 
+#creamos una sub-ruta. ej: 'http://127.0.0.1:5000/products/stivel' la variable nombre va a ser stivel
 @products.route('/<nombre>')
 def get_products(nombre):
     if nombre == "pygroup":
+        #la funcion Response devuelve primero un estrin y despues una respuesta HTTP Response(String,HTTP)
         return Response("ERROR! No se puede usar el nombre pygroup",status=400)
     else:
         return Response("Felicitaciones! Trabajo exitoso {}".format(nombre),status=200)
