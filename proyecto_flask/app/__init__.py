bf447671ae395fed60cf80a77af1e671805db53e
DarcanoS <spinillap@correo.udistrital.edu.co>
2020-11-13 22:50:36 -0500

    Agrego principio de la DB

--- proyecto_flask/app/__init__.py
+++ proyecto_flask/app/__init__.py
@@ -1,13 +1,30 @@
-from flask import Flask, Response
+
 #importamos de la carpeta productos y del archivo models la variable products
-from productos.models import products
+from db import db, ma
+from conf.config import DevelopmentConfig
+from app.products.views import products
 
-app = Flask(__name__)
+ACTIVE_ENDPOINTS = [('/products', products)]
 
 
-#inicializamos la variable products que es tipo Blueprint
-app.register_blueprint(products)
+def create_app(config=DevelopmentConfig):
+    app = Flask(__name__)
 
-#esto no se que hace, pero se debe colocar :'v
+    app.config.from_object(config)
+
+    db.init_app(app)
+    ma.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    # register each active blueprint
+    for url, blueprint in ACTIVE_ENDPOINTS:
+        app.register_blueprint(blueprint, url_prefix=url)
+
+    return app
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app_flask = create_app()
+    app_flask.run()
--- proyecto_flask/app/productos/models.py
+++ proyecto_flask/app/productos/models.py
@@ -1,14 +1,82 @@
-#Importamos el Blueprint y Response
-from flask import  Blueprint, Response
+from datetime import datetime
 
-#Creamos una variable tipo Blueprint, con el nombre 'products' y con la dirrecion de url '/products'
-products = Blueprint('products', __name__, url_prefix='/products')
+from flask import jsonify
 
-#creamos una sub-ruta. ej: 'http://127.0.0.1:5000/products/stivel' la variable nombre va a ser stivel
-@products.route('/<nombre>')
-def get_products(nombre):
-    if nombre == "pygroup":
-        #la funcion Response devuelve primero un estrin y despues una respuesta HTTP Response(String,HTTP)
-        return Response("ERROR! No se puede usar el nombre pygroup",status=400)
-    else:
-        return Response("Felicitaciones! Trabajo exitoso {}".format(nombre),status=200)
+from app.db import db, ma
+
+
+class Product(db.Model):
+    """
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    weight = db.Column(db.Integer, default=1)
+    description = db.Column(db.String(500), nullable=True)
+    refundable = db.Column(db.Boolean, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now())
+
+
+class ProductSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Product
+        fields = ["id", "name", "price"]
+
+
+class Category(db.Model):
+    """
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now())
+
+
+class CategorySchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Category
+
+
+class Stock(db.Model):
+    """
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    quantity = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now())
+
+
+def get_all_categories():
+    categories = Category.query.all()
+    category_schema = CategorySchema()
+    categories = [category_schema.dump(category) for category in categories]
+    return categories
+
+
+def create_new_category(name):
+    category = Category(name=name)
+    db.session.add(category)
+
+    if db.session.commit():
+        return category
+
+    return None
+
+
+def get_all_products():
+    products_qs = Product.query.all()
+    product_schema = ProductSchema()
+    products_serialization = [product_schema.dump(product) for product in
+                              products_qs]
+
+    return products_serialization
+
+
+def get_product_by_id(id):
+    product_qs = Product.query.filter_by(id=id).first()
+    product_schema = ProductSchema()
+    p = product_schema.dump(product_qs)
+    return p
